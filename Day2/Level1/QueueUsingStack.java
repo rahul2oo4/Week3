f5ed5fbbd76c051eb8d6d549e750687cfc058479
rahulr2004 <rahultanikonda@gmail.com>
2025-04-01 10:05:00 +0530

    Add Week3 Day2 Level1 Java files

---
+++ Day2/Level1/QueueUsingStack.java
@@ -0,0 +1,49 @@
+
+import java.util.Stack;
+class QueueUsingStacks {
+ Stack<Integer> stack1 = new Stack<>();
+ Stack<Integer> stack2 = new Stack<>();
+ public void enqueue(int value) {
+  stack1.push(value);
+ }
+ public int dequeue() {
+  if (stack2.isEmpty()) {
+   while (!stack1.isEmpty()) {
+    stack2.push(stack1.pop());
+   }
+  }
+  if (stack2.isEmpty()) {
+   throw new RuntimeException("Queue is empty");
+  }
+  return stack2.pop();
+ }
+ public int peek() {
+  if (stack2.isEmpty()) {
+   while (!stack1.isEmpty()) {
+    stack2.push(stack1.pop());
+   }
+  }
+  if (stack2.isEmpty()) {
+   throw new RuntimeException("Queue is empty");
+  }
+  return stack2.peek();
+ }
+ public boolean isEmpty() {
+  return stack1.isEmpty() && stack2.isEmpty();
+ }
+}
+public class QueueUsingStack {
+ public static void main(String[] args) {
+  QueueUsingStacks queue = new QueueUsingStacks();
+  queue.enqueue(10);
+  queue.enqueue(20);
+  queue.enqueue(30);
+  System.out.println(queue.dequeue());
+  System.out.println(queue.peek());
+  queue.enqueue(40);
+  System.out.println(queue.dequeue());
+  System.out.println(queue.dequeue());
+  System.out.println(queue.dequeue());
+ }
+}
+
